78a22e6389d77cdff27769eb02d70c35ff941e00
sheng <sunwhere0207@gmail.com>
2025-04-25 09:38:53 +0800

    优化路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,26 @@
 import { createBrowserRouter } from "react-router";
 
 import HomeView from "../views/HomeView/index.jsx";
+import HomeViewDetail from "../views/HomeView/Detail/index.jsx";
 import AboutView from "../views/AboutView/index.jsx";
+import AboutViewDetail from "../views/AboutView/Detail/index.jsx";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   Component: App,
-  // },
   {
     path: "/",
     Component: HomeView,
+    children: [
+      // { path: "/HomeView/Detail", Component: HomeViewDetail },
+      { index: true, Component: HomeViewDetail },
+    ],
   },
   {
     path: "/AboutView",
     Component: AboutView,
+    children: [
+      // { path: "/AboutView/Detail", Component: AboutViewDetail },
+      { index: true, Component: AboutViewDetail },
+    ],
   },
 ]);
 
